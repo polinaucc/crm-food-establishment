941350cac8bf5f6d9317f717c9f203e1fc986960
momopoi443 <myzika99993824@gmail.com>
2023-08-09 16:03:32 +0300

    [CRM-4]: Create, run services + Eureka registry

---
+++ eureka/src/test/java/com/crmfoodestablishment/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmfoodestablishment.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
