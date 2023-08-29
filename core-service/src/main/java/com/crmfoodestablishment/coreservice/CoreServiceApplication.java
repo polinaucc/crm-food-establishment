28b2236ac25d8c9cc349f55ccdfe9b51ff579d0e
Zhenya Baraniuk <zhenyabaraniuk@gmail.com>
2023-08-30 01:09:31 +0300

    Dummy commit

--- core-service/src/main/java/com/crmfoodestablishment/coreservice/CoreServiceApplication.java
+++ core-service/src/main/java/com/crmfoodestablishment/coreservice/CoreServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class CoreServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CoreServiceApplication.class, args);
 	}
 
