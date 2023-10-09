e0f9466009d269c7b34ec2461f40ece56d0145ed
Dima013 <028dima@gmail.com>
2023-10-09 22:59:23 +0300

    commit 09_10

--- payment-service/src/main/java/com/crmfoodestablishment/paymentservice/entity/PaymentStage.java
+++ payment-service/src/main/java/com/crmfoodestablishment/paymentservice/entity/PaymentStage.java
@@ -1,17 +1,7 @@
 package com.crmfoodestablishment.paymentservice.entity;
 
 public enum PaymentStage {
-    UNPAID("Unpaid"),
-    WAITING_FOR_PAYMENT("Waiting for Payment"),
-    PAID("Paid");
-
-    private final String displayName;
-
-    PaymentStage(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
+    UNPAID,
+    WAITING_FOR_PAYMENT,
+    PAID
 }
