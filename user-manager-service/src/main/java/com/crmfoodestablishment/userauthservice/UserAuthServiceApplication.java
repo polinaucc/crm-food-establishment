b004f41e5cd87bb864ac3d0a3c56a27953969bac
momopoi443 <myzika99993824@gmail.com>
2023-11-08 02:52:10 +0200

    Prepared branch for pull request

--- user-manager-service/src/main/java/com/crmfoodestablishment/userauthservice/UserAuthServiceApplication.java
+++ user-manager-service/src/main/java/com/crmfoodestablishment/userauthservice/UserAuthServiceApplication.java
@@ -2,12 +2,10 @@ package com.crmfoodestablishment.userauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ConfigurationPropertiesScan
 public class UserAuthServiceApplication {
 
 	public static void main(String[] args) {
--- user-manager-service/src/main/java/com/crmfoodestablishment/userauthservice/authservice/config/jwt/RefreshTokenProperties.java
+++
@@ -1,7 +0,0 @@
-package com.crmfoodestablishment.userauthservice.authservice.config.jwt;
-
-public record RefreshTokenProperties (
-        String publicKey,
-        String secretKey,
-        Long expirationTime
-) {}
