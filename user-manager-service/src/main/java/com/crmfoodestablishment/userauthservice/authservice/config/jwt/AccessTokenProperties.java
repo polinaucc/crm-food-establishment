436066f9bd68a5221ed740a3f025e461dda6a116
momopoi443 <myzika99993824@gmail.com>
2023-11-08 02:52:10 +0200

    Prepared branch for pull request

--- user-manager-service/src/main/java/com/crmfoodestablishment/userauthservice/authservice/config/jwt/AccessTokenProperties.java
+++
@@ -1,7 +0,0 @@
-package com.crmfoodestablishment.userauthservice.authservice.config.jwt;
-
-public record AccessTokenProperties (
-        String publicKey,
-        String secretKey,
-        Long expirationTime
-) {}
--- user-manager-service/src/main/java/com/crmfoodestablishment/userauthservice/usermanager/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.crmfoodestablishment.userauthservice.usermanager.mapper;
-
-import com.crmfoodestablishment.userauthservice.usermanager.dto.UserDTO;
-import com.crmfoodestablishment.userauthservice.usermanager.entity.User;
-
-public interface UserMapper {
-
-    UserDTO mapUserToUserDTO(User user);
-}
