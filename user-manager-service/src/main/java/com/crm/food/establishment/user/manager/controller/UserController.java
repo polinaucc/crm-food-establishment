4242f4af5c327e7e0da243b75524d0a8aa7e5a31
momopoi443 <myzika99993824@gmail.com>
2024-01-05 12:15:36 +0200

    UserController: - removed wildcard imports - refactored code style

--- user-manager-service/src/main/java/com/crm/food/establishment/user/manager/controller/UserController.java
+++ user-manager-service/src/main/java/com/crm/food/establishment/user/manager/controller/UserController.java
@@ -12,7 +12,14 @@ import org.hibernate.validator.constraints.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,70 +31,46 @@ public class UserController {
 
     public static final String USER_PATH = "/api/user";
     public static final String USER_ID_PATH_VAR = "/{userId}";
-    public static final String USER_PATH_ID = USER_PATH + USER_ID_PATH_VAR;
 
     private final UserService userService;
 
     @PostMapping
     public ResponseEntity<RegisterUserResponseDTO> registerUser(
-            @RequestBody
-            @Valid
-            RegisterUserRequestDTO creationData
+            @RequestBody @Valid RegisterUserRequestDTO creationDTO
     ) {
-        RegisterUserResponseDTO registerResponse = userService.register(
-                creationData
-        );
+        RegisterUserResponseDTO registerResponse = userService.register(creationDTO);
 
         return new ResponseEntity<>(registerResponse, HttpStatus.CREATED);
     }
 
     @PutMapping(USER_ID_PATH_VAR)
     public ResponseEntity<Void> updateUser(
-            @PathVariable
-            @UUID
-            String userId,
-            @RequestBody
-            @Valid
-            UpdateUserRequestDTO updatedData
+            @PathVariable @UUID String userId,
+            @RequestBody @Valid UpdateUserRequestDTO updateDTO
     ) {
-        userService.update(
-                java.util.UUID.fromString(userId),
-                updatedData
-        );
+        userService.update(java.util.UUID.fromString(userId), updateDTO);
 
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(USER_ID_PATH_VAR)
-    public ResponseEntity<Void> deleteUser(
-            @PathVariable
-            @UUID
-            String userId
-    ) {
-        userService.delete(
-                java.util.UUID.fromString(userId)
-        );
+    public ResponseEntity<Void> deleteUser(@PathVariable @UUID String userId) {
+        userService.delete(java.util.UUID.fromString(userId));
 
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping(USER_ID_PATH_VAR)
-    public ResponseEntity<UserDTO> getUserById(
-            @PathVariable
-            @UUID
-            String userId
-    ) {
-        UserDTO userResponsePayload = userService.getById(
-                java.util.UUID.fromString(userId)
-        );
+    public ResponseEntity<UserDTO> getUserById(@PathVariable @UUID String userId) {
+        UserDTO fetchedUser = userService.getById(java.util.UUID.fromString(userId));
 
-        return ResponseEntity.ok(userResponsePayload);
+        return ResponseEntity.ok(fetchedUser);
     }
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> listUsers() {
-        List<UserDTO> foundUsers = userService.listAll();
+        List<UserDTO> fetchedUserList = userService.listAll();
 
-        return ResponseEntity.ok(foundUsers);
+        return ResponseEntity.ok(fetchedUserList);
     }
 }
